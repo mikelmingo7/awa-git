13df7b212b27486fe9926e18f65398ff86cfeb75
mikelmingo <mikelmingo@E108L2.opendeusto.es>
2020-10-09 12:43:41 +0200

    Más exclamaciones

--- Awa/src/Awaa.java
+++ Awa/src/Awaa.java
@@ -3,7 +3,7 @@ public class Awaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola chango");
+		System.out.println("hola chango!!!!");
 	}
 	
 }
